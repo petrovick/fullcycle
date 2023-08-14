b4f8d9172cd6dd65b0aad28a54602f8b299aba32
Petrovick <petrovickg@hotmail.com>
2023-08-14 15:33:18 -0300

    remove files and add submodele

--- 21.CleanArquitecture/2.CleanArchitecture/src/domain/product/entity/product.interface.ts
+++
@@ -1,8 +0,0 @@
-export default interface ProductInterface {
-  get id(): string;
-  get name(): string;
-  get price(): number;
-  
-  changeName(name: string) : void;
-  changePrice(price: number): void;
-}
--- 21.CleanArquitecture/2.CleanArchitecture/src/domain/product/repository/productb-repository.interface.ts
+++
@@ -1,4 +0,0 @@
-import RepositoryInterface from "../../@shared/repository/repository-interface";
-import ProductB from "../entity/product-b";
-
-export default interface ProductRepositoryInterface extends RepositoryInterface<ProductB> {}
