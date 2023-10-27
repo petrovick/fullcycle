c2ee2bf497c14c2ec9a7f3085421b90ecc337256
Petrovick <petrovickg@hotmail.com>
2023-10-27 06:53:56 -0300

    add submodule

--- 21.CleanArquitecture/2.CleanArchitecture/src/domain/@shared/event/event.interface.ts
+++
@@ -1,5 +0,0 @@
-export default interface EventInterface {
-    dataTimeOccurred: Date;
-    eventData: any;
-  }
-  
--- 21.CleanArquitecture/2.CleanArchitecture/src/domain/product/repository/product-repository.interface.ts
+++
@@ -1,6 +0,0 @@
-import Product from "../entity/product";
-import RepositoryInterface from "../../@shared/repository/repository-interface";
-import ProductB from "../entity/product-b";
-import ProductInterface from "../entity/product.interface";
-
-export default interface ProductRepositoryInterface extends RepositoryInterface<ProductInterface> {}
